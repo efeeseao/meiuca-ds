c3c231b9ff1957b9b6911d5f69f1f89f6cb1bbed
Fernando dos Santos <nando4803@gmail.com>
2022-02-05 19:05:30 +0100

    test: add heading component

--- src/components/Heading/test.spec.tsx
+++ src/components/Heading/test.spec.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 
-import { Heading } from '.'
+import Heading from '.'
 
 describe('<Heading />', () => {
-  it('should render the heading', () => {
-    const title = 'Hello Heading'
+  it('should render a heading with a small size', () => {
+    render(<Heading size="sm">Button label</Heading>)
 
-    render(<Heading title={title} />)
+    expect(screen.getByRole('heading', { name: /button label/i })).toHaveStyle({
+      'font-size': '20px'
+    })
+  })
 
-    expect(screen.getByText(title)).toBeInTheDocument()
+  it('should render a heading with a medium size', () => {
+    render(<Heading size="md">Button label</Heading>)
+
+    expect(screen.getByRole('heading', { name: /button label/i })).toHaveStyle({
+      'font-size': '24px'
+    })
+  })
+
+  it('should render a heading with a large size', () => {
+    render(<Heading size="lg">Button label</Heading>)
+
+    expect(screen.getByRole('heading', { name: /button label/i })).toHaveStyle({
+      'font-size': '32px'
+    })
   })
 })
